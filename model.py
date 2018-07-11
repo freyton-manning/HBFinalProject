f594143cbeb42ed96ffbf9dc91f873a2eff5c83d
vagrant <vagrant@vagrant.vm>
2018-07-11 00:26:13 +0000

    First Commit

---
+++ model.py
@@ -0,0 +1,146 @@
+"""Models and database functions for Ratings project."""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of mood tracking website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(20), nullable= False)
+    password = db.Column(db.String(64), nullable=False)
+    reminders_on = db.Column(db.Boolean, nullable=False)
+    #age = db.Column(db.Integer, nullable=True)
+    #zipcode = db.Column(db.String(15), nullable=True)
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<User user_id={self.user_id} email={self.email}>"
+
+class User_Moods(db.Model):
+    """User Moods Information - all the data they log"""
+    __tablename__ = "user_moods"
+
+    record_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+    comments = db.Column(db.String(1000), nullable = True)
+    hours_slept = db.Column(db.Float, nullable=True)
+    exercise_mins = db.Column(db.Integer, nullable=True)
+
+    hashtags = db.relationship("Hashtag", secondary = "user_data_hashtags")
+
+class Moods(db.Model):
+    """ Table of available Moods and mood information"""
+    __tablename__ = "moods"
+
+    mood_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    mood_word = db.Column(db.String(25), nullable=False)
+    #Not quite sure what to do with Mood type rn, so keeping it nullable 
+    #Might do initial DB seed without it 
+    mood_type = db.Column(db.String(50), nullable = True)
+
+class UserGoals(db.Model):
+    __tablename__ = "users_goals"
+
+    goal_record_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    frequency = db.Column(db.Integer, nullable=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goals.goal_id'), nullable=False)
+    #Keeping timeframe optional field for now 
+    timeframe = db.Column(db.String(50), nullable=False)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("user_goals"))
+    goal = db.relationship("Goals")
+    # Define relationship to movie
+    #movie = db.relationship("Movie",
+                            #backref=db.backref("ratings",
+                                               #order_by=rating_id))
+
+class Goals(db.Model):
+    """ Table of all goals - predetermined list (ex: fitness)"""
+    __tablename__ = "goals"
+
+    goal_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    goal_description = db.Column(db.String(50), nullable=False)
+
+class Hashtag(db.Model):
+    """ Table of all hashtags with an id, for searching later"""
+    __tablename__ = "hashtags"
+
+    hashtag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    text = db.Column(db.String(25), nullable=False)
+
+class Activities(db.Model):
+    """Ratings information"""
+    __tablename__ = "activities"
+
+    activity_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    #TODO: FIGURE OUT RESPONSE TYPES FROM MEETUP API - ID 
+    meetup_id = db.Column(db.String(100), nullable=True)
+    meetup_url = db.Column(db.String(300), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goals.goal_id'), nullable=False)
+    activity_date = db.Column(db.DateTime, nullable = False)
+    did_attend = db.Column(db.BOOLEAN, nullable = False)
+
+class UserDataHashtags(db.Model):
+    """Ratings information"""
+    __tablename__ = "user_data_hashtags"
+
+    user_hashtag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    hashtag_id = db.Column(db.Integer, db.ForeignKey('hashtags.hashtag_id'), nullable=False)
+    record_id = db.Column(db.Integer, db.ForeignKey('user_moods.record_id'), nullable=False)
+
+    
+
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id} 
+                   movie_id={self.movie_id} 
+                   user_id={self.user_id} 
+                   score={self.score}>"""
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
---
+++ server.py
@@ -0,0 +1,111 @@
+"""Final Project Flask server.
+
+Provides web interface for login, tracking mood / comments / hashtags, and
+viewing mood visualization data. Also lets a user select goals from a menu.
+
+"""
+
+from flask import Flask, render_template, redirect, flash, session
+import jinja2
+
+from model import User, User_Moods, Moods, UserGoals, Goals, Hashtag, Activities, UserDataHashtags, connect_to_db, db
+
+app = Flask(__name__)
+
+# A secret key is needed to use Flask sessioning features
+
+app.secret_key = 'this-should-be-something-unguessable'
+
+# Normally, if you refer to an undefined variable in a Jinja template,
+# Jinja silently ignores this. This makes debugging difficult, so we'll
+# set an attribute of the Jinja environment that says to make this an
+# error.
+
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+
+@app.route("/")
+def homepage():
+    """Return homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route("/register", methods=["GET"])
+def register_form():
+    """Return page showing the registration form
+    Show all fields user needs to fill out in order to register for app
+    """
+    return render_template("registration_form.html")
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    username = request.form.get("username")
+    reminders_on = request.form.get("reminders_on")
+
+    query = User.query.filter(User.email == email).first()
+    
+    if query is None:
+        new_user = User(email=user_email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    return redirect("/")
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+
+    """logs an existing user in after they register"""
+    user_email = request.form.get('email')
+    password = request.form.get('password')
+     #user_id = User.query.filter(User.email)
+
+    #query = User.query.filter(User.email==user_email, User.password == password).first()
+    user = User.query.filter(User.email == user_email).first()
+
+    if user is not None:
+        if user.password == password:
+            session["user_name"] = user.user_id
+            flash("Logged in")
+            return redirect("/")
+        else: 
+            flash("Password is incorrect!")
+            return redirect("/login")
+    else: 
+        flash("Incorrect email, please register")
+        return redirect ("/register")        
+
+
+# @app.route("/melons")
+# def list_melons():
+#     """Return page showing all the melons ubermelon has to offer"""
+
+#     melon_list = melons.get_all()
+#     return render_template("all_melons.html",
+#                            melon_list=melon_list)
+
+
+if __name__ == "__main__":
+    #app.run(debug=True)
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    #DebugToolbarExtension(app)
+
+    app.run(debug=True, port=5000, host='0.0.0.0')
