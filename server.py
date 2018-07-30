4d0f962c2afa2f0f8385e7883ce2d2c3b80d6740
Freya Rajeshwar <frajeshwar@gmail.com>
2018-07-30 06:48:12 +0000

    updated navbar, fixed some rebroken-routes, still stuck on chart.js cuttoff issue and lack of whitepace on form pages

--- server.py
+++ server.py
@@ -214,5 +214,5 @@ if __name__ == "__main__":
     #DebugToolbarExtension(app)
 
     #app.run(debug=True, port=5000, host='0.0.0.0')
-    app.run(debug=True, port=5000, host='127.0.0.1')
+    app.run(debug=True, port=5000, host='0.0.0.0')
 
